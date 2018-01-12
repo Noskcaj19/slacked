7724d9b9a2317285b7937a0bc1babaef316798e9
Noskcaj <kcajdev@gmail.com>
2018-01-11 19:01:34 -0500

    Bare bones WebSocket connection

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use std::io::Read;
 use std::env;
 
 use failure::Error;
+// use failure::Fail;
 
 mod errors;
 mod objects;
@@ -20,16 +21,15 @@ use objects::{Config, Connect};
 use errors::*;
 
 macro_rules! unwrap_or_exit {
-    ($c:expr, $e:expr) => {
+    ($c:expr, $($e:expr),+) => {
         match $c {
             Ok(x) => x,
             Err(e) => {
-                eprintln!($e);
+                eprintln!($($e),+);
                 eprintln!("{}", e.cause());
                 std::process::exit(1);
             }
         };
-
     }
 }
 
@@ -54,8 +54,24 @@ fn get_ws_url(token: &str) -> Result<String, Error> {
     Ok(result.url)
 }
 
+fn connect_ws(ws_url: String) -> Result<(), Error> {
+    ws::connect(ws_url, |out| Client { out: out })?;
+    Ok(())
+}
+
+struct Client {
+    out: ws::Sender,
+}
+
+impl ws::Handler for Client {
+    fn on_message(&mut self, msg: ws::Message) -> Result<(), ws::Error> {
+        println!("< {}", msg);
+        Ok(())
+    }
+}
+
 fn main() {
     let config = unwrap_or_exit!(load_config(), "Error loading config!");
-    let ws_url = unwrap_or_exit!(get_ws_url(config.token), "Error connecting to Slack");
-    println!("{}", ws_url);
+    let ws_url = unwrap_or_exit!(get_ws_url(&config.token), "Error connecting to Slack");
+    unwrap_or_exit!(connect_ws(ws_url), "Error connecting to Slack");
 }
